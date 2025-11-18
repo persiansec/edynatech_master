5514710eb8106b8f292a4f1ce85558747e021437
Ali Nourmohamadi <persiansec@outlook.com>
2025-11-19 02:08:39 +0330

    products delete from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 // app/page.tsx
 import Banner from "@/components/Banner/Banner"
-import ProductsSection from "@/components/Product/ProductsSection"
-import ProductionSection from "@/components/Production/ProductionSection"
 import ContactSection from "@/components/Contact/ContactSection"
 
 export default function Home() {
   return (
     <>
       <Banner />            {/* بنر */}
-      <ProductsSection />   {/* بخش محصولات */}
       <ContactSection />    {/* فرم تماس */}
     </>
   )
